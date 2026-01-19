b0759e3f413667f7055b2f73e01078fd1c808efe
d90905200-lab <d90905200@gmail.com>
2026-01-19 18:19:42 +0300

    styles

--- js/main.js
+++ js/main.js
@@ -1,10 +1,14 @@
 const routes = {
-    '/': {
-        template: '',
+    '/desks': {
+        template: ``,
         script: () => console.log('Desks загружена')
     },
     '/search': {
         template: '<h1>Search</h1>',
+        script: () => console.log('Search загружена')
+    },
+    '/': {
+        template: '<h1>Добро пожаловать!</h1>',
         script: () => console.log('Search загружена')
     },
     '404': {
@@ -12,6 +16,31 @@ const routes = {
         script: () => console.log('Hash не найден')
     }
 };
+const hash_link_list = document.querySelectorAll(".hash_link")
+
+hash_link_list.forEach(link => {
+    link.addEventListener("click", () => {
+        link.classList.add("tools_list_item__active");
+    })
+})
+window.addEventListener("hashchange", () => {
+    hash_link_list.forEach(link => {
+        if (link.hash == window.location.hash) {
+            link.classList.add("tools_list_item__active");
+        } else if (link.classList.contains("tools_list_item__active")) {
+            link.classList.remove("tools_list_item__active");
+        }
+    })
+})
+window.addEventListener("load", () => {
+    hash_link_list.forEach(link => {
+        if (link.hash == window.location.hash) {
+            link.classList.add("tools_list_item__active");
+        } else if (link.classList.contains("tools_list_item__active")) {
+            link.classList.remove("tools_list_item__active");
+        }
+    })
+})
 
 class hashRouter {
     constructor(routes) {
@@ -22,7 +51,7 @@ class hashRouter {
         window.addEventListener("load", () => this.handleRouting())
     }
     handleRouting() {
-        const hash = window.location.hash.substring(1) || '/'
+        let hash = window.location.hash.substring(1) || '/'
             , route = this.routes[hash] || this.routes['404']
         if (route && route.template) {
             document.getElementById('desk_container').innerHTML = route.template
@@ -39,3 +68,4 @@ document.getElementById("desk_columns_list_container").addEventListener("wheel",
         alert("Зажми Shift для прокрутки")
     }
 })
+
