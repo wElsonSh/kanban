518209cbf25de0c5a94d5bd96fc406e2cdfb0ae7
wElsonSh <seveldaiil@gmail.com>
2026-01-29 18:26:19 +0300

    handle column creating

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+const column_name = document.getElementById("column_name")
+    , desk_monitor_list = document.getElementById("desk_monitor_list")
+    , create_column_btn = document.getElementById("create_column_btn")
 let openDB = indexedDB.open("KANBAN", 2)
     , db
 
@@ -18,30 +21,84 @@ openDB.onerror = () => {
 
 openDB.onsuccess = (event) => {
     db = event.target.result
-    alert("БД удалось успешно открыть!", openDB.result)
     console.log(db);
 
-    const transaction = db.transaction("desks", "readwrite")
+
+    broadcastAllData()
+
+    create_column_btn.addEventListener('click', () => {
+        createColumn(prompt("Введи название колонки:"))
+    })
+}
+
+
+const broadcastAllData = () => {
+    let transaction = db.transaction("desks", "readonly")
         , store = transaction.objectStore("desks")
-        , clearRequest = store.clear()
+        , getAllData = store.getAll()
 
-    clearRequest.onsuccess = () => {
-        store.add({ name: "Телефон", price: 10000 })
-    }
+    getAllData.onsuccess = () => {
+        let allDataArray = getAllData.result
+        console.log(allDataArray);
 
-    transaction.oncomplete = () => {
-        console.log('Данные успешно отправлены...');
-
-        const readTransaction = db.transaction("desks", "readonly")
-            , readStore = readTransaction.objectStore("desks")
-            , getAllData = readStore.getAll()
-
-        getAllData.onsuccess = () => {
-            console.log(getAllData.result);
-
-        }
+        desk_monitor_list.innerHTML = ''
+        allDataArray.forEach((data, index) => {
+            createDataHTML(data)
+        })
 
     }
 }
 
+createDataHTML = (data) => {
 
+    let columnBody = document.createElement("li")
+        , columnTegsArr = [...data.tegs]
+        , columnTasksArr = [...data.tasks]
+    console.log(columnTegsArr);
+
+
+    columnBody.className = "column"
+    columnBody.innerHTML = `
+                <div class="column_characteristics">
+                    <div class="column_header">
+                        <span class="column_name" id="column_name">${data.name}</span>
+                        <button class="delete_column_btn"><i class="fa-solid fa-trash"></i></button>
+                    </div>
+                    <ul class="column_tegs" id="column_tegs_${data.id}"></ul>
+                </div>
+                <div class="column_content">
+                    <ul class="column_content_list" id="column_content_list_${data.id}">
+                        <li class="column_task_creator">
+                            <button class="task_creator_btn">
+                                <p>Создать задачу</p>
+                            </button>
+                        </li>
+                    </ul>
+                </div>
+            `
+
+    desk_monitor_list.appendChild(columnBody)
+
+    let column_tegs_ID = document.getElementById(`column_tegs_${data.id}`)
+    columnTegsArr.forEach(teg => {
+        let column_teg = document.createElement("li")
+        column_teg.className = "column_teg"
+        column_teg.textContent = teg
+        column_tegs_ID.appendChild(column_teg)
+    })
+
+    let column_content_list_ID = document.getElementById(`column_content_list_${data.id}`)
+    columnTasksArr.forEach(task => {
+        let column_task = document.createElement("li")
+        column_task.className = "column_content_task"
+        column_task.textContent = task
+        column_content_list_ID.appendChild(column_task)
+    })
+}
+
+createColumn = (ColumnName) => {
+    let transaction = db.transaction("desks", "readwrite")
+        , store = transaction.objectStore("desks")
+    store.add({ name: ColumnName, tegs: [], tasks: [] })
+    broadcastAllData()
+}
