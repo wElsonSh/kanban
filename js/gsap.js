e443a374af6ff9b8aa9d979bd5af7dedfb0db701
wElsonSh <seveldaiil@gmail.com>
2026-01-31 21:59:29 +0300

    rebuilding

---
+++ js/gsap.js
@@ -0,0 +1,4 @@
+let tl = gsap.timeline();
+tl.from(".desks_monitor", { x: -2000, duration: 0.7, ease: "expo.out" })
+    .from(".navbar", { x: - 500, duration: 0.7, ease: "expo.out" })
+
--- js/main.js
+++ js/main.js
@@ -1,10 +1,11 @@
-const column_name = document.getElementById("column_name")
-    , desk_monitor_list = document.getElementById("desk_monitor_list")
-    , create_column_btn = document.getElementById("create_column_btn")
-let openDB = indexedDB.open("KANBAN", 2)
-    , db
-
-
+const openDB = indexedDB.open("kanban", 1)
+    , create_desk_btn = document.querySelector(".create_desk_btn")
+    , navbar_desks_list = document.querySelector(".navbar_desks_list")
+    , desk_monitor_list = document.querySelector(".desk_monitor_list")
+let db
+    , all_desks_btns
+    , hash_s_count
+    , routes = {}
 openDB.onupgradeneeded = (event) => {
     db = event.target.result
     if (!db.objectStoreNames.contains("desks")) {
@@ -13,92 +14,188 @@ openDB.onupgradeneeded = (event) => {
             autoIncrement: true,
         })
     }
-    console.log(db);
+
 }
 openDB.onerror = () => {
-    alert('Чтото пошло не так: ', openDB.error)
+    alert("Ошибка, не удалось открыть БД: " + openDB.error);
+
 }
 
 openDB.onsuccess = (event) => {
     db = event.target.result
-    console.log(db);
+    showAllDataHTML()
 
 
-    broadcastAllData()
 
-    create_column_btn.addEventListener('click', () => {
-        createColumn(prompt("Введи название колонки:"))
+    create_desk_btn.addEventListener("click", () => {
+        createNewDesk(prompt("Введи название доски: "))
+        navbar_desks_list.innerHTML = ""
+        showAllDataHTML()
+    })
+
+    window.addEventListener('hashchange', () => {
+        router()
+    });
+    window.addEventListener("load", () => {
+        router()
     })
 }
 
+const createNewDesk = (DeskName) => {
+    let transaction = db.transaction("desks", "readwrite")
+        , store = transaction.objectStore("desks")
 
-const broadcastAllData = () => {
+    store.add({
+        name: DeskName, columns: [
+            { column_name: "column_1", tasks: [] },
+            { column_name: "column_2", tasks: [] }
+        ]
+    })
+    let get_hash = localStorage.getItem("hash")
+
+
+
+    if (!get_hash) {
+        localStorage.setItem("hash", 0)
+        // console.log(localStorage.getItem("hash"));
+    }
+
+    let old_hash = parseInt(localStorage.getItem("hash"))
+        , new_hash = old_hash + 1
+    localStorage.setItem("hash", new_hash)
+    // console.log(localStorage.getItem("hash"));
+
+}
+
+const showAllDataHTML = () => {
     let transaction = db.transaction("desks", "readonly")
         , store = transaction.objectStore("desks")
-        , getAllData = store.getAll()
+        , readStore = store.getAll()
+    readStore.onsuccess = () => {
+        let readStoreRes = [...readStore.result]
+        console.log(readStoreRes);
 
-    getAllData.onsuccess = () => {
-        let allDataArray = getAllData.result
-        console.log(allDataArray);
 
-        desk_monitor_list.innerHTML = ''
-        allDataArray.forEach((data, index) => {
-            createDataHTML(data)
-        })
+        readStoreRes.forEach(desk => {
+            const newDesk = document.createElement("li")
 
+            newDesk.className = "navbar_desks_list_item"
+            newDesk.innerHTML = `
+                <a href="#/${desk.id}">
+                    <i class="fa-solid fa-folder"></i>
+                    <p>${desk.name}</p>
+                </a>
+            `
+            navbar_desks_list.appendChild(newDesk)
+
+            all_desks_btns = document.querySelectorAll(".navbar_desks_list_item")
+            hash_s_count = desk.id
+            routesCreator(hash_s_count)
+
+        });
+        deskNavBtnsSTYLE()
     }
 }
 
-createDataHTML = (data) => {
+const deskNavBtnsSTYLE = () => {
+    if (all_desks_btns) {
+        all_desks_btns.forEach(btn => {
+            let random_num = Math.floor(Math.random() * 6) + 1;
+            if (random_num == 1) {
+                btn.style.backgroundColor = "#F960D3"
+            } else if (random_num == 2) {
+                btn.style.backgroundColor = "#13D19F"
+            } else if (random_num == 3) {
+                btn.style.backgroundColor = "#F88203"
+            } else if (random_num == 4) {
+                btn.style.backgroundColor = "#19B0E9"
+            } else if (random_num == 5) {
+                btn.style.backgroundColor = "#2B4FFF"
+            } else if (random_num == 6) {
+                btn.style.backgroundColor = "#FF393B"
+            }
 
-    let columnBody = document.createElement("li")
-        , columnTegsArr = [...data.tegs]
-        , columnTasksArr = [...data.tasks]
-    console.log(columnTegsArr);
+        })
+    }
 
-
-    columnBody.className = "column"
-    columnBody.innerHTML = `
-                <div class="column_characteristics">
-                    <div class="column_header">
-                        <span class="column_name" id="column_name">${data.name}</span>
-                        <button class="delete_column_btn"><i class="fa-solid fa-trash"></i></button>
-                    </div>
-                    <ul class="column_tegs" id="column_tegs_${data.id}"></ul>
-                </div>
-                <div class="column_content">
-                    <ul class="column_content_list" id="column_content_list_${data.id}">
-                        <li class="column_task_creator">
-                            <button class="task_creator_btn">
-                                <p>Создать задачу</p>
-                            </button>
-                        </li>
-                    </ul>
-                </div>
-            `
-
-    desk_monitor_list.appendChild(columnBody)
-
-    let column_tegs_ID = document.getElementById(`column_tegs_${data.id}`)
-    columnTegsArr.forEach(teg => {
-        let column_teg = document.createElement("li")
-        column_teg.className = "column_teg"
-        column_teg.textContent = teg
-        column_tegs_ID.appendChild(column_teg)
-    })
-
-    let column_content_list_ID = document.getElementById(`column_content_list_${data.id}`)
-    columnTasksArr.forEach(task => {
-        let column_task = document.createElement("li")
-        column_task.className = "column_content_task"
-        column_task.textContent = task
-        column_content_list_ID.appendChild(column_task)
-    })
 }
 
-createColumn = (ColumnName) => {
-    let transaction = db.transaction("desks", "readwrite")
-        , store = transaction.objectStore("desks")
-    store.add({ name: ColumnName, tegs: [], tasks: [] })
-    broadcastAllData()
+const getWindowHash = () => {
+    let hash = window.location.hash.slice(1)
+    return hash
+}
+
+const routesCreator = (count) => {
+    for (let i = 1; i <= count; i++) {
+        routes[`/${i}`] = `desk ${i}`
+    }
+}
+
+const router = () => {
+    const path = getWindowHash()
+        , s_path = path.slice(1)
+
+    if (routes[path]) {
+        let transaction = db.transaction("desks", "readonly")
+            , store = transaction.objectStore("desks")
+            , getAll = store.getAll()
+        getAll.onsuccess = () => {
+            let getAllRes = [...getAll.result]
+
+            getAllRes.forEach(res => {
+                if (res.id == s_path) {
+                    let columns_arr = [...res.columns]
+
+                    showColumnsHTML(columns_arr)
+                }
+            })
+        }
+
+    } else {
+        desk_monitor_list.innerHTML = "404"
+    }
+
+}
+const showColumnsHTML = (arr) => {
+    desk_monitor_list.innerHTML = ""
+    arr.forEach(col => {
+
+        let column = document.createElement("li")
+        column.className = "column"
+        column.innerHTML = `
+        <div class="column_characteristics">
+            <div class="column_header">
+                <span class='column_name' id='column_name'>${col.column_name}</span>
+                <button class='delete_column_btn'><i class="fa-solid fa-trash"></i></button>
+            </div>
+            <ul class="column_tegs">
+                <li class="column_teg">#teg</li>
+                <li class="column_teg">#home</li>
+                <li class="column_teg">#work</li>
+                <li class="column_teg">#teg</li>
+                <li class="column_teg">#home</li>
+                <li class="column_teg">#work</li>
+            </ul>
+        </div>
+        <div class="column_content">
+            <ul class="column_content_list">
+                <li class="column_task_creator">
+                    <button class="task_creator_btn">
+                        <p>Создать задачу</p>
+                    </button>
+                </li>
+                <li class='column_content_task'>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repudiandae
+                        reiciendis maxime quibusdam temporibus! Tempora quam doloribus eum
+                        consequuntur labore tenetur dignissimos sapiente nobis quae corrupti?</p>
+                </li>
+                <li class='column_content_task'>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis,
+                        ducimus!</p>
+                </li>
+            </ul>
+        </div>
+        `
+        desk_monitor_list.appendChild(column)
+    })
 }
